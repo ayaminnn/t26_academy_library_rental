508fd90d8b7c532aa0f2bc98b9267b449e4b5746
MT 澤 彩未 <ayami.sawa@metateam.co.jp>
2026-05-26 13:30:27 +0900

    貸出登録画面のDB保存とコード追加

--- src/main/java/jp/co/metateam/library/model/Stock.java
+++ src/main/java/jp/co/metateam/library/model/Stock.java
@@ -1,17 +1,16 @@
 package jp.co.metateam.library.model;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
+
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
+
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+
 import jakarta.persistence.Table;
 
 /**
--- src/main/java/jp/co/metateam/library/service/StockService.java
+++ src/main/java/jp/co/metateam/library/service/StockService.java
@@ -1,11 +1,8 @@
 package jp.co.metateam.library.service;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.Locale;
-import java.util.Random;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
